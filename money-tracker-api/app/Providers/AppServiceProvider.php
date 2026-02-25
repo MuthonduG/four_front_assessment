1f3caf2c38c62fc7e6395a8b7a7aa7d8df2449aa
MuthonduG <muthondugithinji@gmail.com>
2026-02-25 17:21:03 +0300

    created routes and updated appservice to prevent csrf block

--- money-tracker-api/app/Providers/AppServiceProvider.php
+++ money-tracker-api/app/Providers/AppServiceProvider.php
@@ -6,19 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Register API routes with the "api" middleware group
+        $this->app->router->group([
+            'prefix' => 'api',
+            'middleware' => 'api',
+        ], function () {
+            require base_path('routes/api.php');
+        });
     }
 }
